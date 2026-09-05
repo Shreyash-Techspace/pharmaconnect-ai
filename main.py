29acc5a3a5d9ca5d0c35c59adad87fce2224af28
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-09-05 23:20:10 +0530

    Add Vercel Serverless Integration and Responsive Sytles.

--- main.py
+++ main.py
@@ -1140,4 +1140,5 @@ async def get_admin_dashboard_data():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
